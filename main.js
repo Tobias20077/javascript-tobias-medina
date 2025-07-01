728222c2668dbdf72d9f1c78a0d4822573732569
Tobias <tobiasmedinaperrotta@gmail.com>
2025-06-30 22:15:39 -0300

    primer leccion

---
+++ main.js
@@ -0,0 +1,23 @@
+// Variables
+// caja que guarda algo
+
+let nombre = "pepe";
+let password = 12345;
+let nombreDos = "Juan";
+
+console.log(nombre);
+prompt("porfavor ingresa un numero");
+alert("muy bien pedazo de basura");
+
+// operadores
+
+let a = 4;
+let b = 12;
+
+console.log(a + b);
+
+// Concatenacion
+
+let nombreUsuario = "Maria";
+let saludo = `hola ${nombreUsuario}  como estas`;
+console.log(saludo);
